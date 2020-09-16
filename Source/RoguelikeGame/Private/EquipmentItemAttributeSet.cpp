8708c2304520f861678360782c9ee694a8b5c0c8
UnArtiste <39284253+UnArtiste@users.noreply.github.com>
2020-09-16 19:08:38 -0400

    EW
    h435GR

---
+++ Source/RoguelikeGame/Private/EquipmentItemAttributeSet.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentItemAttributeSet.h"
+
+
+UEquipmentItemAttributeSet::UEquipmentItemAttributeSet()
+
+
+{
+	EquipmentItemID = 0.0f;
+	EquipmentItemType = 0.0f;
+	EquipmentItemMesh = 0.0f;
+	EquipmentItemVFX = 0.0f;
+	EquipmentItemMainAbilityID = 0.0f;
+	EquipmentItemOffAbilityID = 0.0f;
+	EquipmentItemAttackSpeed = 4.0f;
+	EquipmentItemPhysicalDamage = 1.0f;
+	EquipmentItemFireDamage = 0.0f;
+	EquipmentItemColdDamage = 0.0f;
+	EquipmentItemLightningDamage = 0.0f;
+	EquipmentItemInnateModifierID = 0.0f;
+	EquipmentItemModifier1ID = 0.0f;
+	EquipmentItemModifier2ID = 0.0f;
+	EquipmentItemModifier3ID = 0.0f;
+	EquipmentItemModifier4ID = 0.0f;
+	EquipmentItemModifier5ID = 0.0f;
+	EquipmentItemIsTwoHand = 0.0f;
+	CharacterStance = 0.0f;
+}
---
+++ Source/RoguelikeGame/Public/GameplayEffectTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKEGAME_API UGameplayEffectTest : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
